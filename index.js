d6018a7ca239c829b135a08cd4eb88eac6e73001
Sandro-Moraes <145520414+Sandro-Moraes@users.noreply.github.com>
2023-10-31 20:32:26 -0300

    desafio-finalizado
    Fiz algumas correções quanto ao intervalo de xp e adicionei a variável texto, condensando as informações e deixando o código mais limpo.

--- index.js
+++ index.js
@@ -3,29 +3,30 @@ let nomeHeroi = "Fulaninho" //nome do herói
 let xpHeroi = 1000 // xp do herói
 let mensagemHeroi = "O Herói de nome " // anuncia o nome do herói
 let nivelHeroi = " está no nível de " // anuncia o nível do herói
+let texto = (mensagemHeroi + nomeHeroi + nivelHeroi) // texto visível
 
 // Estrutura de decisão (verifica xp do herói e classifica de acordo com os níveis)
-if (xpHeroi<1000){
-    console.log(mensagemHeroi + nomeHeroi + nivelHeroi + "Ferro")
+if (xpHeroi <= 1000){
+    console.log(texto + "Ferro")
 }
- else if (1001<=xpHeroi<=2000) {
-    console.log(mensagemHeroi + nomeHeroi + nivelHeroi + "Bronze")
+ else if ((xpHeroi >= 1001) && (xpHeroi <= 2000)) {
+    console.log(texto + "Bronze")
 }
- else if (2001<=xpHeroi<=5000){
-    console.log(mensagemHeroi + nomeHeroi + nivelHeroi + "Prata")
+ else if ((xpHeroi >= 2001) && (xpHeroi <= 6000)) {
+    console.log(texto + "Prata")
  }
- else if (6001<=xpHeroi<=7000) {
-    console.log(mensagemHeroi + nomeHeroi + nivelHeroi + "Ouro")
+ else if ((xpHeroi >= 6001) && (xpHeroi <= 7000)) {
+    console.log(texto + "Ouro")
  }
- else if (7001<=xpHeroi<=8000) {
-    console.log(mensagemHeroi + nomeHeroi + nivelHeroi + "Platina")
+ else if ((xpHeroi >= 7001) && (xpHeroi <= 8000)) {
+    console.log(texto + "Platina")
 }
- else if (8001<=xpHeroi<=9000) {
-    console.log(mensagemHeroi + nomeHeroi + nivelHeroi + "Ascendente")
+ else if ((xpHeroi >= 8001) && (xpHeroi <= 9000)) {
+    console.log(texto + "Ascendente")
 }
- else if (9001<=xpHeroi<=10000) {
-    console.log(mensagemHeroi + nomeHeroi + nivelHeroi + "Imortal")
+ else if ((xpHeroi >= 9001) && (xpHeroi <= 10000)) {
+    console.log(texto + "Imortal")
 }
-else if (xpHeroi>=10001) {
-    console.log(mensagemHeroi + nomeHeroi + nivelHeroi + "Radiante")
+else if (xpHeroi >= 10001) {
+    console.log(texto + "Radiante")
 }
